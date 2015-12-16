69351866dbddf05c5f18c4c52973fcf193354ca4
RainWarrior <rainwarrior@fry.su>
2015-12-16 06:03:26 +0300

    Optimization and stuff

--- src/main/java/mezz/jei/gui/ingredients/GuiItemStackFast.java
+++ src/main/java/mezz/jei/gui/ingredients/GuiItemStackFast.java
@@ -1,54 +1,43 @@
 package mezz.jei.gui.ingredients;
 
+import java.util.List;
+
 import javax.annotation.Nonnull;
 import javax.vecmath.Matrix3f;
 import javax.vecmath.Matrix4f;
 import javax.vecmath.Vector3f;
 import javax.vecmath.Vector4f;
-import java.util.List;
-
-import org.apache.commons.lang3.tuple.Pair;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.renderer.EntityRenderer;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.WorldRenderer;
-import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
-import net.minecraft.client.renderer.block.model.ItemTransformVec3f;
-import net.minecraft.client.renderer.entity.RenderItem;
-import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.client.renderer.texture.TextureUtil;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.client.renderer.vertex.VertexFormatElement;
-import net.minecraft.client.resources.model.IBakedModel;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ResourceLocation;
-
-import net.minecraftforge.client.ForgeHooksClient;
-import net.minecraftforge.client.model.IPerspectiveAwareModel;
-import net.minecraftforge.client.model.pipeline.IVertexConsumer;
-import net.minecraftforge.client.model.pipeline.LightUtil;
-import net.minecraftforge.client.model.pipeline.UnpackedBakedQuad;
-import net.minecraftforge.client.model.pipeline.VertexTransformer;
 
 import mezz.jei.config.Config;
 import mezz.jei.config.Constants;
 import mezz.jei.gui.TooltipRenderer;
 import mezz.jei.util.Log;
 import mezz.jei.util.Translator;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.block.model.ItemTransformVec3f;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.renderer.texture.TextureUtil;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+import net.minecraft.client.renderer.vertex.VertexFormatElement;
+import net.minecraft.client.resources.model.IBakedModel;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.client.model.IPerspectiveAwareModel;
+import net.minecraftforge.client.model.pipeline.IVertexConsumer;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 @SuppressWarnings("deprecation")
 public class GuiItemStackFast {
-	private static final ResourceLocation RES_ITEM_GLINT = new ResourceLocation("textures/misc/enchanted_item_glint.png");
 	private static final float RADS = (float) (180.0 / Math.PI);
 
 	private final Matrix4f tempMat = new Matrix4f();
@@ -94,16 +83,15 @@ public class GuiItemStackFast {
 		return (itemStack != null) && (mouseX >= xPosition) && (mouseY >= yPosition) && (mouseX < xPosition + width) && (mouseY < yPosition + height);
 	}
 
-	public void renderItemAndEffectIntoGUI(boolean isGui3d) {
-		if (itemStack == null) {
+	public void renderItemAndEffectIntoGUI(MatrixTransformer transformer, boolean isGui3d, boolean renderEffect) {
+	    if (itemStack == null) {
 			return;
 		}
+	    if(renderEffect && !itemStack.hasEffect()) return;
 
 		if (Config.editModeEnabled) {
 			renderEditMode();
 		}
-
-		GlStateManager.pushMatrix();
 
 		int x = xPosition + padding + 8;
 		int y = yPosition + padding + 8;
@@ -127,7 +115,7 @@ public class GuiItemStackFast {
 			transformMat.mul(tempMat);
 		}
 
-		handleCameraTransforms(transformMat, this.bakedModel, ItemCameraTransforms.TransformType.GUI);
+        handleCameraTransforms(transformMat, this.bakedModel, ItemCameraTransforms.TransformType.GUI);
 
 		tempMat.setIdentity();
 		tempMat.setScale(0.5f);
@@ -142,18 +130,21 @@ public class GuiItemStackFast {
 		invTransposeMat.invert();
 		invTransposeMat.transpose();
 
+		transformer.setTransform(transformMat);
+		transformer.setInvtranspose(invTransposeMat);
 //		ForgeHooksClient.multiplyCurrentGlMatrix(transformMat);
-//
+
 //		transformMat.setIdentity();
 //		invTransposeMat.setIdentity();
 
-		renderModel(transformMat, invTransposeMat, bakedModel, itemStack);
-
-		if (itemStack.hasEffect()) {
-			renderEffect(transformMat, invTransposeMat, bakedModel);
+		if(!renderEffect)
+		{
+		    renderModel(transformer, bakedModel, itemStack);
 		}
-
-		GlStateManager.popMatrix();
+		else
+		{
+		    renderEffect(transformer, bakedModel);
+		}
 	}
 
 	public void handleCameraTransforms(Matrix4f transformMat, IBakedModel model, ItemCameraTransforms.TransformType cameraTransformType) {
@@ -191,117 +182,51 @@ public class GuiItemStackFast {
 		}
 	}
 
-	public static BakedQuad applyToQuad(final Matrix4f transform, final Matrix3f invtranspose, BakedQuad quad) {
-		UnpackedBakedQuad.Builder builder = new UnpackedBakedQuad.Builder(DefaultVertexFormats.ITEM);
-		IVertexConsumer cons = new VertexTransformer(builder)
-		{
-			@Override public void put(int element, float... data)
-			{
-				VertexFormatElement el = DefaultVertexFormats.ITEM.getElement(element);
-
-				switch(el.getUsage())
-				{
-					case POSITION:
-						float[] newData = new float[4];
-						Vector4f vec = new Vector4f(data);
-						transform.transform(vec);
-						vec.get(newData);
-						parent.put(element, newData);
-						break;
-					case NORMAL:
-						float[] newData2 = new float[4];
-						Vector3f vec2 = new Vector3f(data);
-						invtranspose.transform(vec2);
-						vec2.get(newData2);
-						newData2[3] = 0;
-						parent.put(element, newData2);
-						break;
-					default:
-						parent.put(element, data);
-						break;
-				}
-			}
-		};
-		quad.pipe(cons);
-		return builder.build();
+	private void renderModel(MatrixTransformer transformer, IBakedModel model, ItemStack stack) {
+		renderModel(transformer, model, -1, stack);
 	}
 
-	private void renderModel(Matrix4f transformMat, Matrix3f invTransposeMat,IBakedModel model, ItemStack stack) {
-		renderModel(transformMat, invTransposeMat, model, -1, stack);
-	}
-
-	private void renderModel(Matrix4f transformMat, Matrix3f invTransposeMat, IBakedModel model, int color, ItemStack stack) {
-		Tessellator tessellator = Tessellator.getInstance();
-		WorldRenderer worldrenderer = tessellator.getWorldRenderer();
-		worldrenderer.startDrawingQuads();
-		worldrenderer.setVertexFormat(DefaultVertexFormats.ITEM);
-
-		for (EnumFacing enumfacing : EnumFacing.VALUES) {
-			this.renderQuads(transformMat, invTransposeMat, worldrenderer, model.getFaceQuads(enumfacing), color, stack);
+	@SuppressWarnings("unchecked")
+    private void renderModel(MatrixTransformer transformer, IBakedModel model, int color, ItemStack stack) {
+	    /*Tessellator tessellator = Tessellator.getInstance();
+        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+        worldrenderer.startDrawingQuads();
+        worldrenderer.setVertexFormat(DefaultVertexFormats.ITEM);*/
+        for (EnumFacing enumfacing : EnumFacing.VALUES) {
+			this.renderQuads(transformer, model.getFaceQuads(enumfacing), color, stack);
 		}
 
-		this.renderQuads(transformMat, invTransposeMat, worldrenderer, model.getGeneralQuads(), color, stack);
-		tessellator.draw();
+		this.renderQuads(transformer, model.getGeneralQuads(), color, stack);
+		//tessellator.draw();
 	}
 
-	private void renderQuads(Matrix4f transformMat, Matrix3f invTransposeMat, WorldRenderer renderer, List quads, int color, ItemStack stack) {
+	private void renderQuads(MatrixTransformer transformer, List<BakedQuad> quads, int color, ItemStack stack) {
 		boolean flag = color == -1 && stack != null;
-		BakedQuad bakedquad;
-		int j;
+		int auxColor;
 
-		for (Object quad : quads) {
-			bakedquad = (BakedQuad) quad;
-			j = color;
-
-			bakedquad = applyToQuad(transformMat, invTransposeMat, bakedquad);
+		for (BakedQuad bakedquad : quads) {
+			auxColor = color;
 
 			if (flag && bakedquad.hasTintIndex()) {
-				j = stack.getItem().getColorFromItemStack(stack, bakedquad.getTintIndex());
+				auxColor = stack.getItem().getColorFromItemStack(stack, bakedquad.getTintIndex());
 
 				if (EntityRenderer.anaglyphEnable) {
-					j = TextureUtil.anaglyphColor(j);
+					auxColor = TextureUtil.anaglyphColor(auxColor);
 				}
 
-				j |= -16777216;
+				auxColor |= -16777216;
 			}
-			LightUtil.renderQuadColor(renderer, bakedquad, j);
+			transformer.setAuxColor(auxColor);
+			bakedquad.pipe(transformer);
 		}
 	}
 
-	private void renderEffect(Matrix4f transformMat, Matrix3f invTransposeMat, IBakedModel model) {
-		TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
-
-		GlStateManager.depthMask(false);
-		GlStateManager.depthFunc(514);
-		GlStateManager.blendFunc(768, 1);
-		textureManager.bindTexture(RES_ITEM_GLINT);
-		GlStateManager.matrixMode(5890);
-
-		GlStateManager.pushMatrix();
-		GlStateManager.scale(8.0F, 8.0F, 8.0F);
-		float f = (float) (Minecraft.getSystemTime() % 3000L) / 3000.0F / 8.0F;
-		GlStateManager.translate(f, 0.0F, 0.0F);
-		GlStateManager.rotate(-50.0F, 0.0F, 0.0F, 1.0F);
-		this.renderModel(transformMat, invTransposeMat, model, -8372020);
-		GlStateManager.popMatrix();
-
-		GlStateManager.pushMatrix();
-		GlStateManager.scale(8.0F, 8.0F, 8.0F);
-		float f1 = (float) (Minecraft.getSystemTime() % 4873L) / 4873.0F / 8.0F;
-		GlStateManager.translate(-f1, 0.0F, 0.0F);
-		GlStateManager.rotate(10.0F, 0.0F, 0.0F, 1.0F);
-		this.renderModel(transformMat, invTransposeMat, model, -8372020);
-		GlStateManager.popMatrix();
-
-		GlStateManager.matrixMode(5888);
-		GlStateManager.blendFunc(770, 771);
-		GlStateManager.depthFunc(515);
-		GlStateManager.depthMask(true);
-		textureManager.bindTexture(TextureMap.locationBlocksTexture);
+	private void renderEffect(MatrixTransformer transformer, IBakedModel model) {
+		this.renderModel(transformer, model, -8372020);
 	}
 
-	private void renderModel(Matrix4f transformMat, Matrix3f invTransposeMat, IBakedModel model, int color) {
-		this.renderModel(transformMat, invTransposeMat, model, color, null);
+	private void renderModel(MatrixTransformer transformer, IBakedModel model, int color) {
+		this.renderModel(transformer, model, color, null);
 	}
 
 	public void renderSlow() {
@@ -383,4 +308,114 @@ public class GuiItemStackFast {
 
 		return list;
 	}
+
+	public static class MatrixTransformer implements IVertexConsumer
+    {
+	    private final IVertexConsumer parent;
+	    private final Matrix4f transform = new Matrix4f();
+	    private final Matrix3f invtranspose = new Matrix3f();;
+
+	    private boolean colored = false;
+        private int vertices = 0;
+
+        private final float[] auxColor = new float[]{1, 1, 1, 1};
+        private final float[][] buf = new float[3][4];
+        private final Vector4f pos = new Vector4f();
+        private final Vector3f normal = new Vector3f();
+
+        @Override
+        public void setQuadColored()
+        {
+            colored = true;
+        }
+
+        public MatrixTransformer(IVertexConsumer parent)
+        {
+            this.parent = parent;
+        }
+
+        public void setTransform(Matrix4f transform)
+        {
+            this.transform.set(transform);
+        }
+
+        public void setInvtranspose(Matrix3f invtranspose)
+        {
+            this.invtranspose.set(invtranspose);
+        }
+
+        public void setAuxColor(int auxColor)
+        {
+            // magic BGRA -> RGBA
+            this.auxColor[2] = (float)(auxColor & 0xFF) / 0xFF;
+            this.auxColor[1] = (float)((auxColor >>> 8) & 0xFF) / 0xFF;
+            this.auxColor[0] = (float)((auxColor >>> 16) & 0xFF) / 0xFF;
+            this.auxColor[3] = (float)((auxColor >>> 24) & 0xFF) / 0xFF;
+        }
+
+        @Override public void put(int element, float... data)
+        {
+            VertexFormatElement el = DefaultVertexFormats.ITEM.getElement(element);
+
+            if(el.getUsage() == VertexFormatElement.EnumUsage.POSITION)
+            {
+                pos.set(data);
+                if(pos.w == 0) pos.w = 1;
+                transform.transform(pos);
+                pos.get(buf[0]);
+                parent.put(element, buf[0]);
+            }
+            else if(el.getUsage() == VertexFormatElement.EnumUsage.NORMAL)
+            {
+                normal.set(data);
+                invtranspose.transform(normal);
+                normal.normalize();
+                normal.get(buf[1]);
+                buf[1][3] = 0;
+                parent.put(element, buf[1]);
+            }
+            else if(el.getUsage() == VertexFormatElement.EnumUsage.COLOR)
+            {
+                System.arraycopy(auxColor, 0, buf[2], 0, buf[2].length);
+                if(colored)
+                {
+                    for(int i = 0; i < 4; i++)
+                    {
+                        buf[2][i] *= data[i];
+                    }
+                }
+                parent.put(element, buf[2]);
+            }
+            else
+            {
+                parent.put(element, data);
+            }
+            if(element == DefaultVertexFormats.ITEM.getElementCount() - 1)
+            {
+                vertices++;
+                if(vertices == 4)
+                {
+                    vertices = 0;
+                    colored = false;
+                }
+            }
+        }
+
+        public VertexFormat getVertexFormat()
+        {
+            return parent.getVertexFormat();
+        }
+
+        @Override
+        public void setQuadTint(int tint)
+        {
+            parent.setQuadTint(tint);
+        }
+
+        @Override
+        public void setQuadOrientation(EnumFacing orientation)
+        {
+            parent.setQuadOrientation(orientation);
+        }
+    };
 }
--- src/main/java/mezz/jei/gui/ingredients/GuiItemStackFastBatch.java
+++ src/main/java/mezz/jei/gui/ingredients/GuiItemStackFastBatch.java
@@ -2,22 +2,30 @@ package mezz.jei.gui.ingredients;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.item.ItemStack;
-
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.pipeline.LightUtil.ItemConsumer;
+import net.minecraftforge.client.model.pipeline.WorldRendererConsumer;
 import mezz.jei.gui.Focus;
+import mezz.jei.gui.ingredients.GuiItemStackFast.MatrixTransformer;
 import mezz.jei.util.ItemStackElement;
 
 public class GuiItemStackFastBatch {
-	private final List<GuiItemStackFast> renderItemsAll = new ArrayList<>();
+    private static final ResourceLocation RES_ITEM_GLINT = new ResourceLocation("textures/misc/enchanted_item_glint.png");
+    private final List<GuiItemStackFast> renderItemsAll = new ArrayList<>();
 	private final List<GuiItemStackFast> renderItemsBuiltIn = new ArrayList<>();
 	private final List<GuiItemStackFast> renderItems2d = new ArrayList<>();
 	private final List<GuiItemStackFast> renderItems3d = new ArrayList<>();
@@ -66,6 +74,47 @@ public class GuiItemStackFastBatch {
 		return null;
 	}
 
+	private void renderSimpleItems(@Nullable GuiItemStackFast hovered, boolean effect)
+	{
+	 // 3d Items
+        GlStateManager.pushMatrix();
+        {
+            GlStateManager.enableLighting();
+            GlStateManager.scale(20.0F, 20.0F, -20.0F);
+            Tessellator tessellator = Tessellator.getInstance();
+            WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+            worldrenderer.startDrawingQuads();
+            worldrenderer.setVertexFormat(DefaultVertexFormats.ITEM);
+            MatrixTransformer transformer = new MatrixTransformer(new WorldRendererConsumer(worldrenderer));
+            for (GuiItemStackFast guiItemStack : renderItems3d) {
+                if (hovered != guiItemStack) {
+                    guiItemStack.renderItemAndEffectIntoGUI(transformer, true, effect);
+                }
+            }
+            tessellator.draw();
+        }
+        GlStateManager.popMatrix();
+
+        // 2d Items
+        GlStateManager.pushMatrix();
+        {
+            GlStateManager.disableLighting();
+            GlStateManager.scale(32.0F, 32.0F, -32.0F);
+            Tessellator tessellator = Tessellator.getInstance();
+            WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+            worldrenderer.startDrawingQuads();
+            worldrenderer.setVertexFormat(DefaultVertexFormats.ITEM);
+            MatrixTransformer transformer = new MatrixTransformer(new WorldRendererConsumer(worldrenderer));
+            for (GuiItemStackFast guiItemStack : renderItems2d) {
+                if (hovered != guiItemStack) {
+                    guiItemStack.renderItemAndEffectIntoGUI(transformer, false, effect);
+                }
+            }
+            tessellator.draw();
+        }
+        GlStateManager.popMatrix();
+
+	}
 	/** renders all ItemStacks and returns hovered gui item stack for later render pass */
 	@Nullable
 	public GuiItemStackFast render(@Nullable GuiItemStackFast hovered, @Nonnull Minecraft minecraft, boolean isMouseOver, int mouseX, int mouseY) {
@@ -93,32 +142,43 @@ public class GuiItemStackFastBatch {
 		GlStateManager.blendFunc(770, 771);
 		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 
-		// 3d Items
-		GlStateManager.pushMatrix();
-		{
-			GlStateManager.enableLighting();
-			GlStateManager.scale(20.0F, 20.0F, -20.0F);
-			for (GuiItemStackFast guiItemStack : renderItems3d) {
-				if (hovered != guiItemStack) {
-					guiItemStack.renderItemAndEffectIntoGUI(true);
-				}
-			}
-		}
-		GlStateManager.popMatrix();
+        renderSimpleItems(hovered, false);
+		// effects
+		
+		GlStateManager.depthMask(false);
+        GlStateManager.depthFunc(514);
+        GlStateManager.blendFunc(768, 1);
+        textureManager.bindTexture(RES_ITEM_GLINT);
+        GlStateManager.matrixMode(5890);
 
-		// 2d Items
-		GlStateManager.pushMatrix();
-		{
-			GlStateManager.disableLighting();
-			GlStateManager.scale(32.0F, 32.0F, -32.0F);
-			for (GuiItemStackFast guiItemStack : renderItems2d) {
-				if (hovered != guiItemStack) {
-					guiItemStack.renderItemAndEffectIntoGUI(false);
-				}
-			}
-		}
-		GlStateManager.popMatrix();
+        GlStateManager.pushMatrix();
+        GlStateManager.scale(8.0F, 8.0F, 8.0F);
+        float f = (float) (Minecraft.getSystemTime() % 3000L) / 3000.0F / 8.0F;
+        GlStateManager.translate(f, 0.0F, 0.0F);
+        GlStateManager.rotate(-50.0F, 0.0F, 0.0F, 1.0F);
 
+        GlStateManager.matrixMode(5888);
+        renderSimpleItems(hovered, true);
+        GlStateManager.matrixMode(5890);
+
+        GlStateManager.popMatrix();
+
+        GlStateManager.pushMatrix();
+        GlStateManager.scale(8.0F, 8.0F, 8.0F);
+        float f1 = (float) (Minecraft.getSystemTime() % 4873L) / 4873.0F / 8.0F;
+        GlStateManager.translate(-f1, 0.0F, 0.0F);
+        GlStateManager.rotate(10.0F, 0.0F, 0.0F, 1.0F);
+
+        GlStateManager.matrixMode(5888);
+        renderSimpleItems(hovered, true);
+        GlStateManager.matrixMode(5890);
+
+        GlStateManager.popMatrix();
+
+        GlStateManager.matrixMode(5888);
+        GlStateManager.blendFunc(770, 771);
+        GlStateManager.depthFunc(515);
+        GlStateManager.depthMask(true);
 		GlStateManager.disableAlpha();
 		GlStateManager.disableRescaleNormal();
 		GlStateManager.disableLighting();
